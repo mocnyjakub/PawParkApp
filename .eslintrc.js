0cb04dfab8a0688ec6df17fdb9d786b2d960e1c6
mocnyjakub <mocnyjakub@gmail.com>
2024-07-27 19:27:25 +0200

    Add eslint with prettier

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+// https://docs.expo.dev/guides/using-eslint/
+module.exports = {
+  extends: ["expo", "plugin:jest/recommended", "prettier"],
+  plugins: ["jest", "testing-library", "prettier"],
+  env: {
+    "jest/globals": true,
+  },
+  rules: {
+    "prettier/prettier": "error",
+  },
+};
--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,6 +1,8 @@
-import { Text, View } from '@/src/components/Themed';
+import { Text, View } from "@/src/components/Themed";
 
 export default function TabOneScreen() {
+  // I want to fetch data from google maps api and display it here, use useQuery hook from react-query
+
   return (
     <View>
       <Text className="font-bold text-blue-800 text-7xl">Tab 1</Text>
