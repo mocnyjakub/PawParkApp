0fbad1595cbe653972fbb8adc524336db90ecb7c
mocnyjakub <mocnyjakub@gmail.com>
2024-07-27 13:13:46 +0200

    Add basic config

---
+++ src/app/(tabs)/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from '@/src/components/Themed';
+
+export default function TabOneScreen() {
+  return (
+    <View>
+      <Text className="font-bold text-blue-800 text-7xl">Tab 1</Text>
+    </View>
+  );
+}
